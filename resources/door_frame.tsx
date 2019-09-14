4e6ce6fa5e1d9d7110a6655d79b0eb01a6565f64
Ali Sina Iklil <ali.iklil@live.at>
2019-09-14 20:50:50 +0200

    Created Inn

---
+++ resources/door_frame.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="door_frame" tilewidth="32" tileheight="32" tilecount="9" columns="3">
+ <image source="LPC Base Assets/LPC Base Assets/tiles/door_frame.png" width="96" height="96"/>
+</tileset>
---
+++ resources/window2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="window2" tilewidth="32" tileheight="32" tilecount="3" columns="1">
+ <image source="LPC Base Assets/LPC Base Assets/tiles/window2.png" width="32" height="96"/>
+</tileset>
