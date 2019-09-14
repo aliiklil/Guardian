c1116f7722c622fdd0f35e138d688faa517d2784
Ali Sina Iklil <ali.iklil@live.at>
2019-09-14 02:57:35 +0200

    Created city hall

---
+++ resources/castle8.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="castle8" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <image source="castle8.png" width="1024" height="1024"/>
+</tileset>
