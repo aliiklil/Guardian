242194f8c2f0697b929f981d39ace9dc8a7b282c
Ali Sina Iklil <ali.iklil@live.at>
2019-09-16 04:27:08 +0200

    Reworked map

---
+++ resources/alchemist_house.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="alchemist_house" tilewidth="32" tileheight="32" tilecount="100" columns="10">
+ <image source="alchemist_house.png" width="320" height="320"/>
+</tileset>
---
+++ resources/castle8brn.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="castle8brn" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <image source="castle8brn.png" width="1024" height="1024"/>
+</tileset>
