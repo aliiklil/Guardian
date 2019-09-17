f0aac82bfbdc5ab758570d74fa36f61fb71b6897
Ali Sina Iklil <ali.iklil@live.at>
2019-09-17 04:34:04 +0200

    Working on map

---
+++ resources/hunter_house.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="hunter_house" tilewidth="32" tileheight="32" tilecount="108" columns="12">
+ <image source="hunter_house.png" width="384" height="288"/>
+</tileset>
