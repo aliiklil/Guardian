fc5593952dfa1032d2d94698f2fc5e0d29398a6d
Ali Sina Iklil <ali.iklil@live.at>
2019-09-18 21:53:35 +0200

    Monastery completed

---
+++ resources/tempel.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tempel" tilewidth="32" tileheight="32" tilecount="120" columns="12">
+ <image source="tempel.png" width="384" height="320"/>
+</tileset>
