4ebb6170f7b8ce39b391e678494b1e283d04e881
Ali Sina Iklil <ali.iklil@live.at>
2019-09-20 02:40:05 +0200

    Fixed map bug

--- resources/blacksmith.tsx
+++ resources/blacksmith.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="blacksmith" tilewidth="32" tileheight="32" tilecount="126" columns="14">
- <image source="blacksmith.png" width="448" height="288"/>
+<tileset name="blacksmith" tilewidth="32" tileheight="32" tilecount="135" columns="15">
+ <image source="blacksmith.png" width="480" height="288"/>
 </tileset>
